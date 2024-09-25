33a0efb1c951fe541a9b97a0d18f9c54bbadc49d
DeviAngelMik <durmus_mikail@yahoo.de>
2024-09-25 11:50:03 +0200

    update backend.php
    Hashpassword erstellen

--- src/backend.php
+++ src/backend.php
@@ -5,11 +5,16 @@ if ($action === "login")
 {
     $username = $_REQUEST["username"];
     $password = $_REQUEST["password"];
-    password_hash($password, PASSWORD_DEFAULT);
-    console.log($password);
+
+    //Password gehashed und mit passcheck auf richtigkeit geprüft
+    $hashpassword = password_hash($password, PASSWORD_DEFAULT);
+
+    //Passcheck gibt ein Wert aus ob True oder False
+    $passcheck = password_verify($password, $hashpassword);
 
     #if (checkLogin($conn, $username, $password)) {
-    if ($username === $password) {
+    //If Passcheck true dann mach success
+    if ($passcheck === true) {
         $response = [
             "status" => "Success",
             "errorMessage" => NULL,
