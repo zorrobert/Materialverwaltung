8309d007baf38d399a406c86232680935fe0c369
zorrobert <118135271+zorrobert@users.noreply.github.com>
2024-11-06 16:34:39 +0100

    setup devenv

---
+++ devenv.nix
@@ -0,0 +1,62 @@
+{ pkgs, lib, config, inputs, ... }:
+let
+  dbUser = "material";
+  dbPassword = "material";
+  dbName = "material";
+in
+{
+  packages = with pkgs; [ git ];
+
+  enterShell = ''
+    sudo sysctl -w net.ipv4.ip_unprivileged_port_start=0
+    echo "If you installed this project for the first time, please run 'composer install'."
+  '';
+
+  scripts = {
+    load-db.exec = ''
+      echo "Make sure devenv up is running in another window."
+      echo "Importing Database..."
+      mysql -u material -pmaterial material < ./src/database.sql
+      echo "Imported Database."
+    '';
+    caddy-allow-port.exec = ''sudo sysctl -w net.ipv4.ip_unprivileged_port_start=0'';
+  };
+
+  languages.php = {
+    enable = true;
+    fpm.pools.web = {
+      settings = {
+        "pm" = "dynamic";
+        "pm.max_children" = 5;
+        "pm.start_servers" = 2;
+        "pm.min_spare_servers" = 1;
+        "pm.max_spare_servers" = 5;
+      };
+    };
+  };
+
+  # create services
+  services = {
+    caddy = {
+      enable = true;
+      virtualHosts."http://localhost" = {
+        extraConfig = ''
+          root * .
+          php_fastcgi unix/${config.languages.php.fpm.pools.web.socket}
+          file_server
+        '';
+      };
+    };
+    mysql = {
+      enable = true;
+      initialDatabases = [ { name = dbName; } ];
+      ensureUsers = [ {
+        name = dbUser;
+        password = dbPassword;
+        ensurePermissions = { "*.*" = "ALL PRIVILEGES"; };
+      }];
+      settings.mysqld = {
+      };
+    };
+  };
+}
