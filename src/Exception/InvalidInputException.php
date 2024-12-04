f04994a2836cb2a0ab018a710a32e5719f24f786
zorrobert <robert.lotz@mailbox.org>
2024-12-04 20:16:26 +0100

    added item entity + controller added exception listener added item/create and item/list

---
+++ src/Exception/InvalidInputException.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Exception;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class InvalidInputException extends HttpException
+{
+    public function __construct(string $message)
+    {
+        $headers = [];
+        parent::__construct(
+            Response::HTTP_BAD_REQUEST,
+            $message,
+            null,
+            $headers,
+            400
+        );
+    }
+}
