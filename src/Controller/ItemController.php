12894977aa1feeb84bd1b7e06d4e4ab74c07c974
zorrobert <118135271+zorrobert@users.noreply.github.com>
2025-02-21 10:49:18 +0100

    feat: added availability data to item list

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Item;
+use App\Entity\Loan;
 use App\Exception\InvalidInputException;
 use App\Exception\MissingInputException;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -17,11 +19,16 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ItemController extends AbstractController
 {
+    public function __construct(
+        private EntityManagerInterface $em,
+        private SerializerInterface $serializer,
+        private ValidatorInterface $validator,
+    )
+    {
+    }
+
     #[Route('/api/item/create', name: 'app_item_create')]
     public function create(
-        EntityManagerInterface $em,
-        SerializerInterface $serializer,
-        ValidatorInterface $validator
     ): BackendResponse
     {
         $request = Request::createFromGlobals()->getContent();
@@ -34,31 +41,42 @@ class ItemController extends AbstractController
             throw new MissingInputException("not logged in");
         }
 
-        $newItems = $serializer->deserialize($request, Item::class . '[]', 'json');
+        $newItems = $this->serializer->deserialize($request, Item::class . '[]', 'json');
 
-        Item::createItems($newItems, $em, $validator);
+        Item::createItems($newItems, $this->em, $this->validator);
         return new BackendResponse('Successfully created '.sizeof($newItems).' new item(s).', 201);
     }
 
     #[Route('/api/item/list', name: 'app_api_item_list')]
-    public function list(EntityManagerInterface $em, SerializerInterface $serializer): BackendResponse
+    public function list(): Response # BackendResponse
     {
-        $items = $em->getRepository(Item::class)->findAll();
+        $loanRepository = $this->em->getRepository(Loan::class);
+        $itemRepository = $this->em->getRepository(Item::class);
 
-        $list = $serializer->normalize($items);
+        $list = $this->serializer->normalize($itemRepository->findAll());
+
+        foreach ($list as $index => $item) {
+            foreach ($item["loans"] as $loanId) {
+                $loan = $loanRepository->find($loanId);
+                $list[$index]["availability"][] = [
+                    "startDate" => $loan->getStartDate(),
+                    "endDate" => $loan->getEndDate(),
+                ];
+            }
+        }
 
         return new BackendResponse(NULL, 200, $list);
     }
 
     #[Route('/api/item/delete', name: 'app_item_delete')]
-    public function delete(EntityManagerInterface $em): BackendResponse
+    public function delete(): BackendResponse
     {
         $request = Request::createFromGlobals()->getContent();
         $idList = array_filter(json_decode($request)); # filter out NULL values and empty strings
         if (empty($idList)) {
             throw new MissingInputException("Endpoint /item/delete expects an array of Item IDs to delete, none provided.");
         }
-        $itemRepository = $em->getRepository(Item::class);
+        $itemRepository = $this->em->getRepository(Item::class);
 
         foreach ($idList as $id)
         {
@@ -66,15 +84,15 @@ class ItemController extends AbstractController
             if (empty($item)) {
                 throw new MissingInputException("Could not delete items: Item ".$id.' was not found.');
             }
-            $em->remove($item);
+            $this->em->remove($item);
         }
-        $em->flush();
+        $this->em->flush();
 
         return new BackendResponse("Successfully deleted ".sizeof($idList).' items.', 200);
     }
 
     #[Route('/api/item/edit', name: 'app_item_edit')]
-    public function edit(EntityManagerInterface $em, ValidatorInterface $validator): BackendResponse
+    public function edit(): BackendResponse
     {
         $request = Request::createFromGlobals()->getContent();
         if (empty($request)) {
@@ -83,7 +101,7 @@ class ItemController extends AbstractController
 
         $items = json_decode($request, true);
 
-        $itemRepository = $em->getRepository(Item::class);
+        $itemRepository = $this->em->getRepository(Item::class);
 
         $updatedItems = [];
         foreach ($items as $id => $item) {
@@ -98,7 +116,7 @@ class ItemController extends AbstractController
             }
             $updatedItems[] = $updated;
         }
-        Item::editItems($updatedItems, $em, $validator);
+        Item::editItems($updatedItems, $this->em, $this->validator);
 
         return new BackendResponse($request, 200);
     }
