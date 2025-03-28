4491fd0e45f55ceb0e19dfadbffecfe575202ff4
zorrobert <118135271+zorrobert@users.noreply.github.com>
2025-03-28 08:46:22 +0100

    feat: moved all pages to twig templates

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -9,38 +9,29 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class PageController extends AbstractController
 {
     public function __construct(
         private EntityManagerInterface $em,
-        private SerializerInterface $serializer,
-        private ValidatorInterface $validator,
     )
+    {}
+
+    #[Route('', name: 'app_page_index')]
+    public function index(): Response
     {
+        return $this->redirectToRoute('app_page_home');
     }
 
-    #[Route('/profile')]
-    public function profile(): Response
+    #[Route('/home', name: 'app_page_home')]
+    public function home(): Response
     {
-        $user = $this->getUser();
-
-        if (empty($user)) {
-            $data = [];
-        } else {
-            $data =[
-                'name' => $user->getUserIdentifier(),
-            ];
-        }
-
-        return $this->render('page/profile.html.twig', [
-            'user' => $data,
+        return $this->render('page/home.html.twig', [
+            #'user' => $data,
         ]);
     }
 
-    #[Route('/material')]
+    #[Route('/material', name: 'app_page_material')]
     public function material(): Response
     {
         $itemRepository = $this->em->getRepository(Item::class);
@@ -49,6 +40,42 @@ class PageController extends AbstractController
 
         return $this->render('page/material.html.twig', [
             'items' => $items
+        ]);
+    }
+
+    #[Route('/calendar', name: 'app_page_calendar')]
+    public function calendar(): Response
+    {
+        return $this->render('page/calendar.html.twig', [
+            #'user' => $data,
+        ]);
+    }
+
+    #[Route('/profile', name: 'app_page_profile')]
+    public function profile(): Response
+    {
+        $user = $this->getUser();
+
+        if (empty($user)) {
+            $data = [];
+        } else {
+            $data = [
+                'name' => $user->getUserIdentifier(),
+                'gender' => 'test',
+                'firstName' => 'Vorname',
+                'lastName' => 'Nachname',
+                'birthday' => '2005-05-20',
+                'email' => 'test@example.com',
+                'phone' => 'Telefonnummer',
+                'street' => 'straße',
+                'streetNumber' => 'hausnummer',
+                'city' => 'Telefonnummer',
+                'zipcode' => 'Postleitzahl',
+            ];
+        }
+
+        return $this->render('page/profile.html.twig', [
+            'user' => $data,
         ]);
     }
 }
